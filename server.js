97c30eeb0b887635c15acf71363f5e01156ec1f0
Aravind R <aravind.gtr@gmail.com>
2016-10-01 09:42:18 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/../article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
@@ -19,11 +19,11 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
-app.get('/article-two', function (req, res) {
+app.get('/../article-two', function (req, res) {
   res.send("I am Aravind");
 });
 
-app.get('/article-three', function (req, res) {
+app.get('/../article-three', function (req, res) {
   res.send("Ok good bye guys.");
 });
 
